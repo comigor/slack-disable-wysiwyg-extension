18eca11506ab0d7b75ac53b5758cf842e575d050
Igor Borges <igor@borges.dev>
2019-11-21 13:41:20 -0300

    Try multiple times to disable the bad editor

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-javascript:(function() {
+function disableBadEditor() {
+  let REMOVED_BAD_EDITOR = false;
+  try {
     const teamId = window.slackDebug.activeTeamId;
     const redux = window.slackDebug[teamId].redux;
     const experiments = redux.getState().experiments;
     const filteredExperiments = {};
+
     for (const key in experiments) {
-        if (!key.includes('wysiwyg')) {
-            filteredExperiments[key] = experiments[key];
-        }
+      if (!key.includes('wysiwyg')) {
+        filteredExperiments[key] = experiments[key];
+      } else {
+        REMOVED_BAD_EDITOR = true;
+      }
     }
+
     redux.dispatch({
-        type: '[19] Bulk add experiment assignments to redux',
-        payload: filteredExperiments
+      type: '[19] Bulk add experiment assignments to redux',
+      payload: filteredExperiments
     });
-})();
+
+    if (REMOVED_BAD_EDITOR) {
+      console.log('Bad editor disabled successfully!');
+      return;
+    }
+  } catch (e) { }
+
+  console.log('Will try again later');
+  setTimeout(disableBadEditor, 1000);
+}
+
+setTimeout(disableBadEditor, 1000);
